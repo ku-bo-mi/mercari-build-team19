a2d9345df0ebf21bd91d5ed68b2813cd3b146c08
Ling-Yi <z-lingyi-build2022@mercari-partner.com>
2022-05-29 11:39:48 +0900

    add:error handling for backend side

--- python/database.py
+++ python/database.py
@@ -1,65 +1,81 @@
-from contextlib import closing, nullcontext
+from contextlib import closing
+
 import sqlite3
-import hashlib
 
 # file path of the database
+
 filename = '../db/mercari.sqlite3'
 
 """
 Creates database
 """
+
+
 def create_tables():
     with closing(sqlite3.connect(filename)) as db_connect:
         db_cursor = db_connect.cursor()
-        sql = 'CREATE TABLE IF NOT EXISTS requests (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, category STRING, image STRING)'
+        sql = """CREATE TABLE IF NOT EXISTS requests (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name STRING, 
+        category STRING,
+         image STRING)"""
         db_cursor.execute(sql)
-        sql = 'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, category STRING, image STRING)'
+        sql = """CREATE TABLE IF NOT EXISTS items (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name STRING, 
+                category STRING, 
+                image STRING)"""
         db_cursor.execute(sql)
         requests = db_cursor.fetchall()
         db_connect.commit()
     return requests
 
+
 """
 Returns the list of all items in the database
 """
-def get_items():
-    items = []
 
+
+def get_items():
     with closing(sqlite3.connect(filename)) as db_connect:
         db_cursor = db_connect.cursor()
-        # insert new data
+        db_connect.row_factory = sqlite3.Row
         sql = 'SELECT * FROM items'
         db_cursor.execute(sql)
         items = db_cursor.fetchall()
-        db_connect.commit()
+        r = [dict((db_cursor.description[i][0], value)
+                  for i, value in enumerate(row)) for row in items]
 
-    return items
+        db_connect.commit()
+        return {'items': r} if r else {'message': 'No data found m(_ _)m'}
+
 
 """
 Search item with the given id
 Return the item if present, return None otherwise
 """
-def get_id_by_id(item_id):
-    item = []
 
+
+def get_id_by_id(item_id):
     with closing(sqlite3.connect(filename)) as db_connect:
         db_cursor = db_connect.cursor()
-        # insert new data
+        db_connect.row_factory = sqlite3.Row
         sql = 'SELECT * FROM items WHERE id = ?'
         data = (item_id,)
         db_cursor.execute(sql, data)
-        item = db_cursor.fetchone()
+        item = db_cursor.fetchall()
+        r = [dict((db_cursor.description[i][0], value)
+                  for i, value in enumerate(row)) for row in item]
         db_connect.commit()
-
-    return item
-
+        return {'items': r} if r else {'message': 'Item not found m(_ _)m'}
 
 
 """
 Add a new item with the given name, category and image to the database
 """
-def add_item(name, category, image_hash):
 
+
+def add_item(name, category, image_hash):
     with closing(sqlite3.connect(filename)) as db_connect:
         db_cursor = db_connect.cursor()
         sql = 'INSERT INTO items(name, category, image) values (?, ?, ?)'
@@ -67,44 +83,53 @@ def add_item(name, category, image_hash):
         db_cursor.execute(sql, data)
         db_connect.commit()
 
+
 """
 Search items with the given string keyword from the database.
 Returns the list of items where its name contains the keyword.
 """
-def search_items(keyword):
-    items = []
 
+
+def search_items(keyword):
     with closing(sqlite3.connect(filename)) as db_connect:
         db_cursor = db_connect.cursor()
+        db_connect.row_factory = sqlite3.Row
         sql = 'SELECT * FROM items WHERE name LIKE ?'
         data = ('%' + keyword + '%',)
         db_cursor.execute(sql, data)
         items = db_cursor.fetchall()
+        r = [dict((db_cursor.description[i][0], value)
+                  for i, value in enumerate(row)) for row in items]
         db_connect.commit()
 
-    return items
+        return {'items': r} if r else {'message': 'Item not found m(_ _)m'}
+
 
 """
 Returns the list of all items in the database
 """
-def get_requests():
-    requests = []
 
+
+def get_requests():
     with closing(sqlite3.connect(filename)) as db_connect:
         db_cursor = db_connect.cursor()
-        # insert new data
+        db_connect.row_factory = sqlite3.Row
         sql = 'SELECT * FROM requests'
         db_cursor.execute(sql)
         requests = db_cursor.fetchall()
+        r = [dict((db_cursor.description[i][0], value)
+                  for i, value in enumerate(row)) for row in requests]
         db_connect.commit()
 
-    return requests
+        return {'items': r} if r else {'message': 'Request not found m(_ _)m'}
+
 
 """
 Add a new request with the given name, category and image to the database
 """
-def add_request(name, category, image_hash):
 
+
+def add_request(name, category, image_hash):
     with closing(sqlite3.connect(filename)) as db_connect:
         db_cursor = db_connect.cursor()
         sql = 'INSERT INTO requests(name, category, image) values (?, ?, ?)'
--- python/main.py
+++ python/main.py
@@ -1,11 +1,12 @@
 import os
 import logging
 import pathlib
-import sqlite3
+
+from sqlite3 import Error
 from fastapi import FastAPI, Form, File, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-# import json
+
 import database
 import hashlib
 
@@ -13,12 +14,12 @@ app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "images"
-origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
+origins = [os.environ.get('FRONT_URL', 'http://localhost:3000')]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
-    allow_methods=["GET","POST","PUT","DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
@@ -27,46 +28,56 @@ Creates tables
 """
 database.create_tables()
 
+"""
+Main page
+"""
 
-"""
-Gets the list of all items
-"""
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
+
 """
 Gets the list of all items
 """
+
+
 @app.get("/items")
 def read_items():
     # get the list of all items in the database
-    items = database.get_items()
-    # format the list and return
-    return format_items(items)
+    try:
+        items = database.get_items()
+        # format the list and return
+        return items
+    except Error as e:
+        logger.error(e)
+        return {'message': f'{e}'}
+
 
 """
 Gets item with the given item_id
 """
+
+
 @app.get("/items/{item_id}")
 async def get_item_by_id(item_id):
-
-    item = database.get_id_by_id(item_id)
-    
-    if not item:
-        logger.debug(f"Item not found: {item_id}")
-        return f"Item not found: {item_id}"
-    
-    return format_item(item)
+    try:
+        item = database.get_id_by_id(item_id)
+        return item
+    except Error as e:
+        logger.error(e)
+        return {'message': f'{e}'}
 
 
 """
 Creates a new item with the given name, cateogry, image
 Accepts the arguments as File.
 """
+
+
 @app.post("/items")
 def add_item(name: bytes = File(...), category: bytes = File(...), image: bytes = File(...)):
-
     # cast bytes to string
     name = name.decode('utf-8')
     category = category.decode('utf-8')
@@ -78,19 +89,26 @@ def add_item(name: bytes = File(...), category: bytes = File(...), image: bytes 
     logger.info(f"Created file: {filename_hash}")
 
     # add a new item in the database with the hashed filename
-    database.add_item(name, category, filename_hash)
-    
-    # # return message
-    return {"message": f"item received: {name}"}
+    try:
+        database.add_item(name, category, filename_hash)
+        return {"message": f"item received: {name}"}
+    except Error as e:
+        logger.error(e)
+        return {'message': f'{e}'}
+
 
 @app.get("/search")
 async def search_items(keyword: str):
     logger.info(f"Receive search_keyword: keyword = {keyword}")
 
     # get the list of items with name that contains the given keyword
-    items = database.search_items(keyword)
+    try:
+        items = database.search_items(keyword)
+        return items
+    except Error as e:
+        logger.error(e)
+        return {'message': f'{e}'}
 
-    return format_items(items)
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
@@ -106,58 +124,51 @@ async def get_image(image_filename):
 
     return FileResponse(image)
 
-"""
-Formats the given list of tuples for printing
-"""
-def format_items(items):
-    # create a list to set each item in a format
-    items_format = []
-    for item in items:
-        item_format = format_item(item)
-        # item_format = {"name": item[0], "category": item[1], "image": item[2]}
-        items_format.append(item_format)
-    # return items_format
-    # return {"items": f"{items_format}"}
-    return {"items": items_format}
-
-"""
-Formats the given item
-"""
-def format_item(item):
-    return {"id": item[0], "name": item[1], "category": item[2], "image": item[3]}
 
 """
 Saves the given bytes of the image file as a new file in "items" directory
 Creates the hash from the given bytes and uses it as the filename
 """
-def save_image(image_bytes):
 
+
+def save_image(image_bytes):
     # hash the bytes with sha256, and put '.jpg' in the end
     filename_hash = hashlib.sha256(image_bytes).hexdigest() + '.jpg'
-    
-    # write the given bytes to a new file
-    with open("images/" + filename_hash, "wb") as fout:
-        fout.write(image_bytes)
 
-    return filename_hash
+    # write the given bytes to a new file
+    try:
+        with open("images/" + filename_hash, "wb") as fout:
+            fout.write(image_bytes)
+            return filename_hash
+    except Error as e:
+        logger.error(e)
+
 
 """
 Gets the list of all requests
 """
+
+
 @app.get("/requests")
 def read_requests():
     # get the list of all items in the database
-    requests = database.get_requests()
+    try:
+        requests = database.get_requests()
     # format the list and return
-    return format_items(requests)
+        return requests
+    except Error as e:
+        logger.error(e)
+        return {'message': f'{e}'}
+
 
 """
 Creates a new requests with the given name, cateogry, image
 Accepts the arguments as File.
 """
+
+
 @app.post("/requests")
 def add_item(name: bytes = File(...), category: bytes = File(...), image: bytes = File(default=None)):
-
     # cast bytes to string
     name = name.decode('utf-8')
     category = category.decode('utf-8')
@@ -175,7 +186,9 @@ def add_item(name: bytes = File(...), category: bytes = File(...), image: bytes 
         filename_hash = ""
 
     # add a new item in the database with the hashed filename
-    database.add_request(name, category, filename_hash)
-    
-    # # return message
-    return {"message": f"item received: {name}"}
+    try:
+        database.add_request(name, category, filename_hash)
+        return {"message": f"item received: {name}"}
+    except Error as e:
+        logger.error(e)
+        return {'message': f'{e}'}
